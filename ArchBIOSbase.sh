735e1f91fb84bd6a44107c65f9ebc41047b7451f
Chaotic-Lab <49621391+Chaotic-Lab@users.noreply.github.com>
2020-08-11 23:56:09 -0500

    Update ArchBIOSbase.sh

--- ArchBIOSbase.sh
+++ ArchBIOSbase.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #This is ChaoticGuru's Arch Linux Scripted Install.
-#Visit "https://github.com/ChaoticHackingNetwork/BasicScripts" for more info
-#This is for BIOS systems...
+#Visit "https://github.com/ChaoticHackingNetwork/BasicScripts/" for more info
+# BIOS systems install...
 
-echo "ChaoticGuru's Arch Linux BIOS Install"
+echo -e "\033[33;7mChaoticGuru's Arch Linux BIOS Install\033[0m"
 
 #Network Connections
 read -p 'Are you connected to the Internet? [y/N]: ' connected
@@ -18,9 +18,9 @@ fi
 echo "This script will create and format the following partitions:"
 echo "/dev/sda1 - 512M will be mounted as /boot"
 echo "/dev/sda2 - 16G will be used as SWAP"
-echo "/dev/sda3 - rest of space will be /mnt"
-read -p 'Continue? [y/N]: ' part
-if ! [ $part = 'y' ] && ! [ $part = 'Y' ]
+echo "/dev/sda3 - rest of space will be /mnt(root)"
+read -p 'Continue? [y/N]: ' partition
+if ! [ $partition = 'y' ] && ! [ $partition = 'Y' ]
 then
 	echo "Please edit the script to continue..."
 	exit
@@ -28,7 +28,7 @@ fi
 
 #Create partitions thru fdisk...
 #https://superuser.com/a/984637
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda # CHANGE THIS IF NEEDED!!!
 	o # Clear the in-memory partition table
 	n # New partition
 	p # Primary partition
@@ -51,20 +51,20 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
 EOF
 
 #Format partitions
-mkfs.ext4 /dev/sda1
 mkfs.ext4 /dev/sda3
+mkfs.ext4 /dev/sda1
+
+#Mount partitions
+mount /dev/sda3 /mnt/boot
+mount /dev/sda1 /mnt
 
 #Create swap space
 mkswap /dev/sda2
 swapon /dev/sda2
 
-#Mount partitions
-mount /dev/sda3 /mnt
-mount /dev/sda1 /mnt/boot
-
 #Display new tables and confirm
 lsblk
-echo "Are the mount points labled and correct?"
+echo "Are the mount points correct?"
 read -p 'Continue? [Y/n]' confirm
 if ! [ $confirm = 'y' ] && ! [ $confirm = 'Y' ]
 then 
@@ -78,17 +78,21 @@ pacman-key --populate archlinux
 pacman-key --refresh-keys
 
 #Install base system
-pacstrap /mnt base base-devel linux linux-firmware vi vim nano
+pacstrap /mnt base base-devel linux linux-firmware
 
 #Generate fstab
-genfstab -U -p /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
 
+#Finish last minute setup
 echo 0 > /proc/sys/kernel/hung_task_timeout_secs
-echo "You will now chroot into your new system : arch-chroot /mnt"
-echo "*** Ensure you download the chrootInstall.sh from https://raw.githubusercontent.com/ChaoticHackingNetwork ***"
-echo "You can use wget or curl or whatever your prefer"
-echo "Run the command: arch-chroot /mnt and then install the chrootInstall script"
-
+wget https://raw.githubusercontent.com/ChaoticHackingNetwork/BasicScripts/master/chrootInstallBIOS.sh
+mv chrootInstallBIOS.sh /mnt
+echo "The next script (chrootInstallBIOS.sh) has been moved to your new root directory..."
+echo "Run the following commands to finish setup...
+echo 
+echo "	[1] arch-chroot /mnt /bin/bash"
+echo "	[2] chmod +x chrootInstallBIOS.sh"
+echo "  [3] ./chrootInstallBIOS.sh
 
 exit
