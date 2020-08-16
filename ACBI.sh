dff9af629e727d31f9fd30b6e64262669d829d53
Chaotic Guru <49621391+Chaotic-Lab@users.noreply.github.com>
2020-08-16 09:35:14 -0500

    Update ACBI.sh

--- ACBI.sh
+++ ACBI.sh
@@ -2,7 +2,7 @@
 
 # Arch Chroot BIOS Install (ACBI)
 # ---------------------------------------------------------------
-# Author    : ChaoticGuru                                        |
+# Author    : Chaotic_Guru                                        |
 # Github    : https://github.com/Chaotic-Lab                     |
 #	            https://github.com/ChaoticHackingNetwork           |
 # Discord   : https://discord.gg/nv445EX (ChaoticHackingNetwork) |
