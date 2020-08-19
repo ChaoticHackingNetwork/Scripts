2cac590af567703a1b68c0a7ee93ab48162a1d66
Chaotic Guru <49621391+Chaotic-Lab@users.noreply.github.com>
2020-08-18 23:27:41 -0500

    Update TCPclient.py

--- TCPclient.py
+++ TCPclient.py
@@ -1,41 +1,24 @@
-# This is a Basic TCP Server
+# This is a simple TCP Client
 # Updated to Python3
 
-import threading
 import socket
 
-# Specifify IP & Port we want Server to listen on
-bind_ip = "0.0.0.0"
-bind_port = 8000
+target_host = "0.0.0.0" # or hostname
+target_port = 8000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Socket Object
+# AF_INET parameter is Standard IPv4 Address
+# SOCK_STREAM is indicating that this is a TCP Client
+# SOCK_DGRAM is UDP
+client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind((bind_ip, bind_port))
+# Connect the Client
+client.connect((target_host, target_port))
 
-# Tell the server to listen with a Maximum backlog of connections set to 2
-server.listen(2)
+# Sending Data
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 
-print("[*] Listening on %s%d" % (bind_ip, bind_port))
+# Recieve Data
+data = client.recv(4096)
+print(data)
 
-# Client-Handling Thread
-def handle_client(client_socket):
-
-         # Print what the client sends
-         request = client_socket.recv(1024)
-         print("[*] Recieved: %s" % request)
-
-         # Send back a packet
-         client_socket.send("ACK!")
-
-         client_socket.close()
-
-# Put Server ino main loop, Awaiting incoming connections!
-while True:
-
-        client,addr = server.accept()
-
-        print("[*] Accepted connection via %s%d" % (addr[0],addr[1]))
-
-        # Spin up our Client Thread to handle incoming Data!
-        client_handler = threading.Thread(target=handle_client,args=(client,))
-        client_handler.start()
