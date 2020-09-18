1a2afff713c1475af48570489d147360b7729288
Chaotic Guru <49621391+Chaotic-Lab@users.noreply.github.com>
2020-09-17 21:03:31 -0500

    Update ABI.sh

--- ABI.sh
+++ ABI.sh
@@ -86,7 +86,7 @@ cat /mnt/etc/fstab
 
 #Finish last minute setup
 echo 0 > /proc/sys/kernel/hung_task_timeout_secs
-wget https://raw.githubusercontent.com/ChaoticHackingNetwork/Scripts/master/ACBI.sh
+curl -O https://raw.githubusercontent.com/ChaoticHackingNetwork/Scripts/master/ACBI.sh
 mv ACBI.sh /mnt
 echo "The next script (ACBI.sh) has been moved to your new root directory..."
 echo "Run the following commands to finish setup..."
