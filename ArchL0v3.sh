cb894e677777830f477d4f6f64fe767ef94b9c44
Chaotic Guru <49621391+Chaotic-Lab@users.noreply.github.com>
2020-09-23 17:41:47 -0500

    Update ArchL0v3.sh

--- ArchL0v3.sh
+++ ArchL0v3.sh
@@ -93,14 +93,14 @@ cat /mnt/etc/fstab
 
 #Finish last minute setup
 echo 0 > /proc/sys/kernel/hung_task_timeout_secs
-curl -O https://raw.githubusercontent.com/ChaoticHackingNetwork/Scripts/master/ACBI.sh
+curl -O https://raw.githubusercontent.com/ChaoticHackingNetwork/Scripts/master/ArchL0v3Final.sh
 mv ACBI.sh /mnt
 echo "The next script (ACBI.sh) has been moved to your new root directory..."
 echo "Run the following commands to finish setup..."
 echo 
 echo "	[1] arch-chroot /mnt /bin/bash"
-echo "	[2] chmod +x ACBI.sh"
-echo "  [3] ./ACBI.sh"
+echo "	[2] chmod +x ArchL0v3Final.sh"
+echo "  [3] ./ArchL0v3Final.sh"
 
 exit
 
